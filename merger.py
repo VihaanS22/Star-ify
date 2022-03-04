5f96e38006be9129f0eb558904754d1c851d9a0b
VihaanS22 <69805124+VihaanS22@users.noreply.github.com>
2022-03-04 12:01:36 +0530

    Add files via upload

---
+++ merger.py
@@ -0,0 +1,39 @@
+import csv
+import pandas as pd
+
+brights = 'brights.csv'
+dwarfs = 'dwarfs.csv'
+
+df1 = []
+df2 = []
+
+with open(brights) as f:
+    csv_reader =csv.reader(f)
+    
+    for i in csv_reader:
+        df1.append(i)
+        
+with open(dwarfs) as f:
+    csv_reader = csv.reader(f)
+    
+    for i in csv_reader:
+        df2.append(i)
+
+brights_headers = df1[0]
+brights_data = df1[1:]
+
+dwarfs_headers = df2[0]
+dwarfs_data = df2[1:]
+
+final_headers = brights_headers + dwarfs_headers
+
+final_data =[]
+
+for index, data_row in enumerate(brights_data):
+    final_data.append(brights_data[index] + dwarfs_data[index])
+
+with open("final_stars.csv",'w') as f:
+    csvwriter = csv.writer(f)
+    csvwriter.writerow(final_headers)   
+    csvwriter.writerows(final_data)
+
